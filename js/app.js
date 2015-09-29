bf3e6505e58dbe26287585a7e3c80568cde38ed8
pedidosmobile <andres.arevalo@grouppangea.com>
2015-09-29 15:34:35 -0500

    Cambios generales

--- js/app.js
+++ js/app.js
@@ -8,16 +8,18 @@
 
      bindEvents: function() { 
          document.addEventListener('deviceready', this.onDeviceReady, false); 
+         //this.onDeviceReady();
      },
 
      onDeviceReady: function() { 
        navigator.notification.alert('OK!');
        this.Conectar_BD();
-       this.ExecuteQuery(this.Crear_BD());
+       this.ExecuteQuery(this.Crear_BD);
      }, 
      
      Conectar_BD: function() {
-       this.db = window.sqlitePlugin.openDatabase('PedidosMobileDB', '1.0', 'Base de datos de pedidos online version mobile', 2*1024*1024);
+       navigator.notification.alert('OK!');
+       this.db = openDatabase('PedidosMobileDB11', '1.0', 'Base de datos de pedidos online version mobile', 2*1024*1024);
        navigator.notification.alert('CONECTADO!');
  	 },
 
@@ -27,7 +29,8 @@
             this.Conectar_BD();
         }
         navigator.notification.alert('EJECUTADO CONSULTA!');
-        this.db.transaction(operacion, function(){navigator.notification.alert('OK!');}, function(){navigator.notification.alert('NOK!');});
+        this.db.transaction(operacion, function(tx, err){navigator.notification.alert('NOK!');}, function(){navigator.notification.alert('OK!');});
+        navigator.notification.alert('EJECUTADO CONSULTA444!');
      },
 
  	 Crear_BD: function(tx){

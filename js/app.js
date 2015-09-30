6f89be8ff3912fd984fc29dbde2ff826ea9ce145
pedidosmobile <andres.arevalo@grouppangea.com>
2015-09-30 14:20:41 -0500

    ACtualizacion
    eeee

--- js/app.js
+++ js/app.js
@@ -8,46 +8,27 @@
 
      bindEvents: function() { 
          document.addEventListener('deviceready', this.onDeviceReady, false); 
-         //this.onDeviceReady();
      },
 
      onDeviceReady: function() { 
       
-       navigator.notification.alert('You are the winner!');
-       //this.Conectar_BD();
-       //
+        navigator.notification.alert('You are the winner!');
 
-       this.db = window.sqlitePlugin.openDatabase({name: "PedidosMobileDB.db"});
+        this.db = window.sqlitePlugin.openDatabase({name: "PedidosMobileDB.db"});
 
-       this.db.transaction(function(tx) {
-          tx.executeSql('DROP TABLE IF EXISTS test_table2');
-          tx.executeSql('CREATE TABLE IF NOT EXISTS test_table2 (id integer primary key, data text, data_num integer)');
+        // this.db.transaction(function(tx) {
+        //      tx.executeSql('DROP TABLE IF EXISTS test_table2');
+        //      tx.executeSql('CREATE TABLE IF NOT EXISTS test_table2 (id integer primary key, data text, data_num integer)');
 
-          // demonstrate PRAGMA:
-          this.db.executeSql("PRAGMA foreign_keys=1;", [], function(res) {
-            navigator.notification.alert("PRAGMA res: " + JSON.stringify(res));
-          }); 
-      });
-
+        //      // demonstrate PRAGMA:
+        //      // this.db.executeSql("PRAGMA foreign_keys=1;", [], function(res) {
+        //      //    navigator.notification.alert("PRAGMA res: " + JSON.stringify(res));
+        //      // }); 
+        //    });
+		
+		this.ExecuteQuery(this.Crear_BD);
         navigator.notification.alert('You are the winner88888!');
      }, 
-     
-     Conectar_BD: function() {
-       navigator.notification.alert('OK!');
-       this.db = window.sqlitePlugin.openDatabase({name: "PedidosMobileDB.db"});
-       //window.sqlitePlugin.openDatabase('PedidosMobileDB', '1.0', 'Base de datos de pedidos online version mobile', 500);
-       navigator.notification.alert('CONECTADO!');
- 	 },
-
-     ExecuteQuery: function(operacion)
-     {
-        if (this.db == null) {
-            this.Conectar_BD();
-        }
-        navigator.notification.alert('EJECUTADO CONSULTA!');
-        this.db.transaction(operacion, function(tx, err){navigator.notification.alert('NOK!');}, function(){navigator.notification.alert('OK!');});
-        navigator.notification.alert('EJECUTADO CONSULTA444!');
-     },
 
  	 Crear_BD: function(tx){
  	   tx.executeSql('PRAGMA foreign_keys = ON');
@@ -61,5 +42,13 @@
 	   tx.executeSql('CREATE TABLE IF NOT EXISTS sucursal (suc_id int,suc_nombre varchar(45),ter_id int, suc_tipo varchar(45),suc_direccion varchar(45),suc_telefono1 varchar(45),suc_telefono2 varchar(45),suc_codigoPostal varchar(45),suc_ciudad varchar(45),suc_depto varchar(45),suc_pais varchar(45),suc_nombreContacto varchar(45),suc_mailCcontacto varchar(45),suc_condicionPago varchar(45),suc_bloqueoCupo varchar(45),suc_bloqueomora varchar(45),suc_cupoCredito varchar(45),suc_estado varchar(45),suc_fechaCreacion varchar(45),suc_usuarioCreacion  varchar(45))');
 	   tx.executeSql('CREATE TABLE IF NOT EXISTS tercero (ter id integer PRIMARY key asc, ter_identificacion varchar(45),ter_tipoIdentificacion  varchar(45),ter_razonSocial  varchar(45),ter_estado varchar(45),ter_esVendedor varchar(45), ter_esCliente varchar(45),ter_esProveedor  varchar(45),ter_fechaCreacion varchar(45),ter_usuario_Creacion varchar(45))');
 	   tx.executeSql('CREATE TABLE IF NOT EXISTS usuario (usu_id integer PRIMARY key asc, usu_cedula varchar(45),usu_nombre varchar(45),usu_indActivo varchar(45),usu_username varchar(45),usu_password varchar(45),usu_empresa varchar(45),usu_nitEmpresa varchar(45),usu_fechaCreacion varchar(45),usu_usuarioCreacion  varchar(45))');
- 	}     
+ 	},
+
+ 	ExecuteQuery: function(operacion)
+    {
+      if (this.db == null) {
+          this.db = window.sqlitePlugin.openDatabase({name: "PedidosMobileDB.db"});
+      }
+      this.db.transaction(operacion, function(tx, err){navigator.notification.alert('NOK!');}, function(){navigator.notification.alert('OK!');});
+    }
  }; 

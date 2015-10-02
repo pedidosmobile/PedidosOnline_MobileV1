d1884c23904568022b609fda5631929e945fb341
pedidosmobile <andres.arevalo@grouppangea.com>
2015-10-02 02:41:07 -0500

    EEE
    EE

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@
       //app.process('GET','http://riapira2289-001-site1.smarterasp.net/DataMobile_Service.svc/Web/GetCustomerList',app.onCliente); 
 	    //db = window.sqlitePlugin.openDatabase("PedidosMobileDB", "1.0", "Pedidos mobile DB", 200000);
       //db.transaction(app.Crear_BD, app.errorCB, app.successCB);
-      app.process('GET','https://api.mercadolibre.com/sites/MLA/search?q=ipod',"");
+      app.process('GET','https://api.mercadolibre.com/sites/MLA/search?q=ipod',app.onCliente);
    },
 
     /* metodo :     String - GET / POST

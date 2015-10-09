9689be21691de525796d564c9e9ac63d8fe63d9c
pedidosmobile <andres.arevalo@grouppangea.com>
2015-10-09 11:58:27 -0500

    plplpl
    plpl

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -60,32 +60,11 @@ var app = {
 
     //alert("Base de datos creada", {},"Operación ok");
 
-            var xhr;
-				var select_state, $select_state;
-				var select_city, $select_city;
+        var xhr;
+		var select_state, $select_state;
+		var select_city, $select_city;
 
-				$select_state = $('#select-state').selectize({
-					onChange: function(value) {
-						if (!value.length) return;
-						select_city.disable();
-						select_city.clearOptions();
-						select_city.load(function(callback) {
-							xhr && xhr.abort();
-							xhr = $.ajax({
-								url: 'http://www.corsproxy.com/api.sba.gov/geodata/primary_city_links_for_state_of/' + value + '.json',
-								success: function(results) {
-									select_city.enable();
-									callback(results);
-								},
-								error: function() {
-									callback();
-								}
-							})
-						});
-					}
-				});
-
-				select_state = $select_state[0].selectize;
+		$select_state = $('#select-state').selectize();
 
           $.ajax({
                  url: "http://riapira2289-001-site1.smarterasp.net/DataMobile_Service.svc/Web/GetCustomerList",

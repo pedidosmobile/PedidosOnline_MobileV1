ac20cc96cc513994ede77d6eb56c94cd9d732dc4
pedidosmobile <andres.arevalo@grouppangea.com>
2015-10-09 08:48:30 -0500

    Commit

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -59,7 +59,22 @@ var app = {
     successCB: function() {
 
     alert("Base de datos creada", {},"Operación ok");
-              $.ajax({
+
+        $('#hero-demo').autoComplete({
+           minChars: 1,
+           source: function(term, suggest){
+              term = term.toLowerCase();
+              var choices = ['ActionScript', 'AppleScript', 'Asp', 'Assembly', 'BASIC', 'Batch', 'C', 'C++', 'CSS', 'Clojure', 'COBOL', 'ColdFusion', 'Erlang', 'Fortran', 'Groovy', 'Haskell', 'HTML', 'Java', 'JavaScript', 'Lisp', 'Perl', 'PHP', 'PowerShell', 'Python', 'Ruby', 'Scala', 'Scheme', 'SQL', 'TeX', 'XML'];
+              var suggestions = [];
+                
+              for(i=0;i<choices.length;i++){
+	   		      if (~choices[i].toLowerCase().indexOf(term)){ suggestions.push(choices[i]);}
+              }
+              suggest(suggestions);
+            }
+        });
+
+          $.ajax({
                  url: "http://riapira2289-001-site1.smarterasp.net/DataMobile_Service.svc/Web/GetCustomerList",
                  type: "GET",
                  data: "{}",

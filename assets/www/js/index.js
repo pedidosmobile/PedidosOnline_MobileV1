227a0a02d04989aa024fec183f92ce4948cfc6bb
pedidosmobile <andres.arevalo@grouppangea.com>
2015-10-09 11:40:05 -0500

    retert
    retert

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -59,7 +59,33 @@ var app = {
     successCB: function() {
 
     //alert("Base de datos creada", {},"Operación ok");
-      $(".js-example-basic-multiple").select2();
+
+            var xhr;
+				var select_state, $select_state;
+				var select_city, $select_city;
+
+				$select_state = $('#select-state').selectize({
+					onChange: function(value) {
+						if (!value.length) return;
+						select_city.disable();
+						select_city.clearOptions();
+						select_city.load(function(callback) {
+							xhr && xhr.abort();
+							xhr = $.ajax({
+								url: 'http://www.corsproxy.com/api.sba.gov/geodata/primary_city_links_for_state_of/' + value + '.json',
+								success: function(results) {
+									select_city.enable();
+									callback(results);
+								},
+								error: function() {
+									callback();
+								}
+							})
+						});
+					}
+				});
+
+				select_state = $select_state[0].selectize;
 
           $.ajax({
                  url: "http://riapira2289-001-site1.smarterasp.net/DataMobile_Service.svc/Web/GetCustomerList",

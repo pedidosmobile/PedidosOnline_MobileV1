220151241a2f9196a3adee0bc2bdb83b3e9b0f20
erwin-hamid <erwinpardoq@hotmail.com>
2015-10-13 16:08:56 -0500

    sincronizacion test

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -6,7 +6,10 @@ cuerpo = document.getElementById("cuerpo"),
 menuprincipal = document.getElementById("menuprincipal"),
 wrapper = document.getElementById("wrapper");
 
+var conexion = window.sqlitePlugin.openDatabase("PedidosMobileDB", "1.0", "Pedidos mobile DB", 200000);
+
 var xhReq = new XMLHttpRequest();
+var query = '';
 
 var app = {
 
@@ -50,13 +53,21 @@ var app = {
         //document.addEventListener('deviceready', this.onDeviceReady, false);
     },
 
+    
+
     onDeviceReady: function() {
     	// Ejecutamos la funci—n FastClick, que es la que nos elimina esos 300ms de espera al hacer click
     	new FastClick(document.body);
-    	this.successCB();
+    	//this.successCB();
+        db = window.sqlitePlugin.openDatabase("PedidosMobileDB", "1.0", "Pedidos mobile DB", 200000);
+        db.transaction(app.Crear_BD, app.errorCB, app.successCB, app.insertRecord);
     },
-    successCB: function() {
 
+    errorCB : function(err) {
+        alert("Error en la operación "+err, {},"Operación fallo");
+    },
+
+    successCB: function() {
     alert("Base de datos creada", {},"Operación ok");
               $.ajax({
                  url: "http://riapira2289-001-site1.smarterasp.net/DataMobile_Service.svc/Web/GetCustomerList",
@@ -90,7 +101,49 @@ var app = {
                 }
          });
    },
+
+   Crear_BD: function(tx){
+       tx.executeSql('CREATE TABLE IF NOT EXISTS descuento(des_id INTEGER PRIMARY KEY ASC, ter_id int, item_id int,des_valorDescuento varchar(45),des_porcentaje varchar(45),des_estado varchar(45),des_fechaCreacion vachar(45),des_usuarioCreacion varchar(45))');
+       tx.executeSql('CREATE TABLE IF NOT EXISTS item (item_id integer primary key asc,item_referencia varchar(45),item_codigo varchar(45),item_descripcion varchar(45),item_cantidadBase varchar(45),item_stock varchar(45),item_estado varchar(45),item_fechaCreacion varchar(45),item_usuarioCreacion varchar(45))');   
+       tx.executeSql('CREATE TABLE IF NOT EXISTS maestro (mae_id integer primary key asc,mae_tipo varchar(45),mae_descripcion varchar(45),mae_moneda varchar(45),mae_fechaCreacion varchar(45),mae_usuarioCreacion varchar(45),mae_estado varchar(45),usu_id int,suc_id int)');
+       tx.executeSql('CREATE TABLE IF NOT EXISTS pedido (ped_id integer primary key asc,ter_id int,mae_id int,pto_id int,ped_fechapedido varchar(45),ped_fechaEntrega varchar(45),ped_observaciones varchar(45),ped_observaciones2 varchar(45),ped_ordenCompra varchar(45),ped_referencia varchar(45),ped_valorNeto varchar(45),ped_descuento varchar(45),ped_impuesto varchar(45),ped_valorTotal varchar(45),ped_valorFacturado varchar(45),ped_estado varchar(45),ped_nroPedidoERP varchar(45),ped_nroFacturaERP varchar(45),ped_estadoERP varchar(45),ped_medioDePago varchar(45),ped_estadoDePago varchar(45),ped_saldo varchar(45))');
+       tx.executeSql('CREATE TABLE IF NOT EXISTS pedido_detalle (pdet_id integer primary key ASC,ped_id int,mae_id int,item_id int,pdet_descripcion varchar(45),pdet_cantidad varchar(45),pdet_valorImpuesto varchar(45),pdet_porcentajeDescuento  varchar(45),pdet_descuento varchar(45),pdet_valorNeto varchar(45),pdet_fechaCreacion varchar(45),pdet_usuarioCreacion varchar(45))');
+       tx.executeSql('CREATE TABLE IF NOT EXISTS precio (pre_id integer primary key ASC,item_id int,mae_id int,pre_unidad varchar(45),pre_precio varchar(45),pre_factor varchar(45),pre_fechaCreacion varchar(45),pre_usuarioCreacion varchar (45))');
+       tx.executeSql('CREATE TABLE IF NOT EXISTS punto_envio (pto_id integer PRIMARY KEY asc,pto_nombre varchar(45),pto_estado varchar(45),pto_fechaCreacion varchar(45),pto_usuarioCreacion varchar(45),suc_id int)');
+       tx.executeSql('CREATE TABLE IF NOT EXISTS sucursal (suc_id int,suc_nombre varchar(45),ter_id int, suc_tipo varchar(45),suc_direccion varchar(45),suc_telefono1 varchar(45),suc_telefono2 varchar(45),suc_codigoPostal varchar(45),suc_ciudad varchar(45),suc_depto varchar(45),suc_pais varchar(45),suc_nombreContacto varchar(45),suc_mailCcontacto varchar(45),suc_condicionPago varchar(45),suc_bloqueoCupo varchar(45),suc_bloqueomora varchar(45),suc_cupoCredito varchar(45),suc_estado varchar(45),suc_fechaCreacion varchar(45),suc_usuarioCreacion  varchar(45))');
+       tx.executeSql('CREATE TABLE IF NOT EXISTS tercero (ter id integer PRIMARY key asc, ter_identificacion varchar(45),ter_tipoIdentificacion  varchar(45),ter_razonSocial  varchar(45),ter_estado varchar(45),ter_esVendedor varchar(45), ter_esCliente varchar(45),ter_esProveedor  varchar(45),ter_fechaCreacion varchar(45),ter_usuario_Creacion varchar(45))');
+       tx.executeSql('CREATE TABLE IF NOT EXISTS usuario (usu_id integer PRIMARY key asc, usu_cedula varchar(45),usu_nombre varchar(45),usu_indActivo varchar(45),usu_username varchar(45),usu_password varchar(45),usu_empresa varchar(45),usu_nitEmpresa varchar(45),usu_fechaCreacion varchar(45),usu_usuarioCreacion  varchar(45))');
+       db.transaction(app.insertRecord);
+    },
+
+    insertRecord: function(tx){
+        
+        $.ajax({
+                 url: "http://riapira2289-001-site1.smarterasp.net/DataMobile_Service.svc/Web/GetListaPedidos",
+                 type: "GET",
+                 cache: false,
+                 data: "{}",
+                 contentType: "application/json; charset=utf-8",
+                 dataType: "json",
+                 processData: true,
+                 success: function (data) {
+
+                     var dataparse = JSON.parse(data);
+
+                     query = 'INSERT INTO pedido(ped_nroPedidoERP, ped_fechapedido, ped_observaciones, ped_valorTotal) values("123", "' + dataparse[0]['Fecha_Pedido'] + '", "' + dataparse[0]['Sucursal'] + '", "' + dataparse[0]['Valor'] + '")'
+                     SINCRONIZAR_NUEVOUSUARIO(query);
+                     //tx.executeSql(query);
+                 },
+                 error: function (response) {
+                     console.log("Error");
+                     console.log(response.statusCode);
+                 }
+             });
+        tx.executeSql(query);
+    }
 };
+
+
 
 // Funci—n para a–adir clases css a elementos
 function addClass( classname, element ) {
@@ -172,6 +225,7 @@ function submenu(opcion){
 }
 
 function GetListaPedidos() {
+        //myScroll = new iScroll('wrapper', { hideScrollbar: false });
              $.ajax({
                  url: "http://riapira2289-001-site1.smarterasp.net/DataMobile_Service.svc/Web/GetListaPedidos",
                  type: "GET",
@@ -185,7 +239,7 @@ function GetListaPedidos() {
                      var dataparse = JSON.parse(data);
 
                      //console.log(dataparse);
-                     myScroll = new iScroll('wrapper', { hideScrollbar: false });
+
                      var x = 1;
 
                      var lista = $('#divPedidos');
@@ -208,7 +262,7 @@ function GetListaPedidos() {
                                  $('<span/>').text('SUCURSAL: ' + dataparse[i]['Sucursal']).addClass('meta expiry'),
                                  $('<span/>').text('$' + dataparse[i]['Valor']).addClass('meta cost'),
                                  $('<i/>').addClass('chevron')
-                             )
+                             )//.attr("href", "http://www.google.com/")
                              )
                          
                          x = x + 1;
@@ -225,3 +279,9 @@ function GetListaPedidos() {
                  }
              });
          }
+
+function SINCRONIZAR_NUEVOUSUARIO(query){
+ self.conexion.transaction(function(tx,rs){
+    tx.executeSql(query);
+ });
+}
